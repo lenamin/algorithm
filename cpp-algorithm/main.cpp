345d50d405b1ae648a0220975a0166c13e9d5705
lena <lenaminn@gmail.com>
2023-01-31 19:14:12 +0900

    Initial Commit

---
+++ cpp-algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-algorithm
+//
+//  Created by Lena on 2023/01/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
